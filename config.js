66840fc49bfcd7726f8d63b2fd369fc37c7b6740
Micheal <michealfriesen@gmail.com>
2018-05-23 21:44:28 -0600

    This thing is a mess and in the process of transfering to HapiJS
    Added a bunch of things and removed a bunch of things. Basically switching the default express from the spotify tutorial to hapi.js. MEGA WIP
    
    Fixed stupid mistake of keys after reseting them... live an learn!

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
-// The main config file for the project. Meant to keep all keys involved secure and off git.
 const config = {};
 
 config.keys = {};
 
 // Spotify API Keys
-config.keys.client_id = '*** YOUR CLIENT ID HERE***'; // Your client id
-config.keys.client_secret = '*** YOUR CLIENT SECRET HERE***'; // Your secret
-config.keys.redirect_uri = 'http://localhost:8888/callback'; // Default redirect uri
+config.keys.client_id = '37d52c6069384dd2825ae4519eb1f436'; // Your client id
+config.keys.client_secret = 'a8824e9ad0444785a5ef604d2da657c8'; // Your secret
+config.keys.redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 module.exports = config;
